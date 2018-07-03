0cf09780b7fc81a393082a27d52129b533afcbd3
César Gutiérrez Yáñez <cesargy@gmail.com>
2018-07-02 23:10:27 -0500

    Asteroid constructor created

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 const totalAsteroids = 5;
 
+const Asteroid = function (imageIndex, top) {
+
+  this.getAsteroid = function () {
+    const newAsteroid = document.createElement('img');
+    newAsteroid.classList.add('asteroid');
+    newAsteroid.style.top = top + 'px';
+    newAsteroid.src = 'assets/asteroid_' + imageIndex + '.png';
+
+    return newAsteroid;
+  };
+
+};
+
 function addAsteroids(total) {
   for(let i = 0; i < total; i++) {
     const rdmAsteroid = Math.ceil(Math.random() * 3);
     const top = Math.ceil( Math.random() * (window.innerHeight - 150 ) );
 
+    /* Old way to create an Asteroid
     const newAsteroid = document.createElement('img');
     newAsteroid.classList.add('asteroid');
     newAsteroid.style.top = top + 'px';
-    newAsteroid.src = 'assets/asteroid_' + rdmAsteroid + '.png';
+    newAsteroid.src = 'assets/asteroid_' + rdmAsteroid + '.png';*/
 
-    document.querySelector('.game-wrapper').appendChild(newAsteroid);
+    // New way to create an asteroid using constructors
+    const newAsteroid = new Asteroid(rdmAsteroid, top);
+
+    document.querySelector('.game-wrapper').appendChild(newAsteroid.getAsteroid());
   }
 }
 
